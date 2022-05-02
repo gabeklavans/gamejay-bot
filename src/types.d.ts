52209a7fbe2e7f92598b50f499594e554af00714
Gabriel <gabeklav@gmail.com>
2022-05-01 20:24:10 -0400

    Switch to using pf-boggle to generate and solve boards

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,16 @@
+declare module "pf-boggle" {
+	function generate(size: number = 4, dice: any[] = []): string[];
+	function solve(
+		board: string[],
+		dictionary: string[] = []
+	): { word: string; sequence: number[] }[];
+
+	export { generate, solve };
+}
+
 type Tile = {
-    letter: string;
-    container?: Phaser.GameObjects.Container;
-    row: number;
-    col: number;
+	letter: string;
+	container?: Phaser.GameObjects.Container;
+	row: number;
+	col: number;
 };
--- src/word-hunt/main.ts
+++ src/word-hunt/main.ts
@@ -1,13 +1,8 @@
-import { TextFrequency } from "../correct-frequency-random-letters/index";
-const letterGenerator = new TextFrequency();
+import pfBoggle from "pf-boggle";
 
 const DEBUG = true;
-const GRID_SIZE = 3;
-const tileGrid: Tile[][] = [];
-
-for (let i = 0; i < GRID_SIZE; i++) {
-	tileGrid.push([]);
-}
+const GRID_SIZE = 4;
+let board: string[] = [];
 
 export async function init() {
 	let boardWords = new Set<string>();
@@ -16,105 +11,18 @@ export async function init() {
 		if (DEBUG) {
 			console.log("generating new board...");
 		}
-
-		for (let row = 0; row < GRID_SIZE; row++) {
-			for (let col = 0; col < GRID_SIZE; col++) {
-				tileGrid[row][col] = {
-					letter: letterGenerator.random(),
-					row: row,
-					col: col,
-				};
-			}
-		}
-
-		// get all the words in the board
-		boardWords = await getWordsRecursion();
-		if (DEBUG) {
-			// eslint-disable-next-line no-console
-			console.log(boardWords);
-		}
+		board = pfBoggle.generate(GRID_SIZE);
+		const solutions = pfBoggle.solve(board);
+		boardWords = new Set(
+			solutions
+				.map((solution) => solution.word)
+				.filter((word) => word.length > 2)
+		);
 	}
 
 	if (DEBUG) {
+		console.log(boardWords);
 		// eslint-disable-next-line no-console
-		console.log(tileGrid);
+		console.log(board);
 	}
-}
-
-async function getWordsRecursion() {
-	const boardWords = new Set<string>();
-	const dict = await loadSpellCheck();
-	for (const destTile of tileGrid.flat()) {
-		const foundWords = new Set<string>();
-		getWordsRecursionHelper(
-			destTile,
-			new Set<Tile>(),
-			"",
-			foundWords,
-			dict
-		);
-		console.log(`found for ${destTile.letter}:`);
-		console.log(foundWords);
-		foundWords.forEach((foundWord) => boardWords.add(foundWord));
-	}
-
-	return boardWords;
-}
-
-function getWordsRecursionHelper(
-	source: Tile,
-	visited: Set<Tile>,
-	word: string,
-	foundWords: Set<string>,
-	dict: Set<string>
-) {
-	visited.add(source);
-	word += source.letter;
-	if (word.length >= 3 && dict.has(word.toLowerCase())) {
-		foundWords.add(word);
-	}
-
-	for (const neighbor of getTileNeighbors(source.row, source.col)) {
-		if (!visited.has(neighbor)) {
-			getWordsRecursionHelper(neighbor, visited, word, foundWords, dict);
-		}
-	}
-
-	word = word[word.length - 1];
-	visited.delete(source);
-}
-
-// TODO: add a supplementary dict withwords such as:
-// TITS
-async function loadSpellCheck() {
-	// const wordList = await fetch("assets/2of12.txt").then((response) =>
-	// 	response.text()
-	// );
-	const wordList = "asfd";
-	const wordArr = wordList.split("\r\n");
-
-	return new Set(wordArr);
-}
-
-function getTileNeighbors(row: number, col: number) {
-	const neighbors: Tile[] = [];
-	for (let rowOffset = -1; rowOffset <= 1; rowOffset++) {
-		for (let colOffset = -1; colOffset <= 1; colOffset++) {
-			// don't add self as neighbor
-			if (!(rowOffset == 0 && colOffset == 0)) {
-				const neighborRow = row + rowOffset;
-				const neighborCol = col + colOffset;
-				if (
-					neighborRow >= 0 &&
-					neighborRow < GRID_SIZE &&
-					neighborCol >= 0 &&
-					neighborCol < GRID_SIZE
-				) {
-					neighbors.push(tileGrid[neighborRow][neighborCol]);
-				}
-			}
-		}
-	}
-
-	return neighbors;
 }
