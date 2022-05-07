e0fef0c28c64f9a2fce417aaa5625baf9302d267
Gabriel <gabeklav@gmail.com>
2022-05-07 13:29:26 -0400

    Test sending game callback

--- src/bot.ts
+++ src/bot.ts
@@ -9,7 +9,15 @@ const bot = new Bot(process.env.BOT_API_KEY!);
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
+bot.command("game", async (ctx) => {
+	await ctx.replyWithGame("WordHunt");
+});
+
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+bot.on("callback_query:game_short_name", async (ctx) => {
+	await ctx.answerCallbackQuery({ url: "http://192.168.0.42:8081" });
+});
 
 export default function startBot() {
 	bot.start();
