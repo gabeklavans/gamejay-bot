905c5b655bb4450992186af6a423193575aae0df
Gabriel <gabeklav@gmail.com>
2022-06-05 19:01:28 -0400

    Trying to expose server

--- src/server.ts
+++ src/server.ts
@@ -133,7 +133,7 @@ fastify.post<{
 export default async function startServer() {
 	await who.init();
 
-	fastify.listen(3000, (err) => {
+	fastify.listen(3000, "192.168.1.2", (err) => {
 		if (err) {
 			fastify.log.error(err);
 			process.exit(1);
