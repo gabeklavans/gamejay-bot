61eced9f0e6cd76e72ff1462b2c0ddc1cea7d5a9
Gabriel Klavans <gabeklav@fastmail.com>
2022-06-24 18:13:52 -0400

    Add timestamps to log

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-	apps : [{
-    		name   : "GameJay Bot",
-    		script : "npm",
-		args   : "start",
-  	}]
-}
+	apps: [
+		{
+			name: "GameJay Bot",
+			script: "npm",
+			args: "start",
+			log_date_format: "MM-DD-YY HH:mm",
+		},
+	],
+};
